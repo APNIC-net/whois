c5ad3c38d0f1d658d82da9b593d2c3e419bb7619
dragank <dragan@apnic.net>
2013-06-06 17:13:03 +1000

    Update code from broken merge

--- whois-update/src/main/java/net/apnic/db/whois/update/authentication/strategy/AutnumAuthentication.java
+++ whois-update/src/main/java/net/apnic/db/whois/update/authentication/strategy/AutnumAuthentication.java
@@ -1,6 +1,9 @@
 package net.apnic.db.whois.update.authentication.strategy;
 
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import net.ripe.db.whois.common.Message;
 import net.ripe.db.whois.common.dao.RpslObjectDao;
 import net.ripe.db.whois.common.domain.CIString;
 import net.ripe.db.whois.common.profiles.WhoisVariant;
@@ -15,11 +18,14 @@ import net.ripe.db.whois.update.domain.Action;
 import net.ripe.db.whois.update.domain.PreparedUpdate;
 import net.ripe.db.whois.update.domain.UpdateContext;
 import net.ripe.db.whois.update.domain.UpdateMessages;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 @WhoisVariantContext(includeWhen = WhoisVariant.Type.APNIC)
 @Component
@@ -38,30 +44,47 @@ public class AutnumAuthentication implements AuthenticationStrategy {
         return update.getType().equals(ObjectType.AUT_NUM) && update.getAction().equals(Action.CREATE);
     }
 
-    // @TODO: Customise as per SCRUM-1549
+    @Override
+    public Set<ObjectType> getPendingAuthenticationTypes() {
+        return Collections.emptySet();
+    }
+
     @Override
     public List<RpslObject> authenticate(final PreparedUpdate update, final UpdateContext updateContext) {
         final RpslObject object = update.getUpdatedObject();
 
         final CIString pkey = object.getKey();
         final long number = Long.parseLong(pkey.toString().substring("AS".length()));
-        final RpslObject asBlock = objectDao.findAsBlock(number, number);
-        if (asBlock == null) {
+
+        List<RpslObject> asBlockList = objectDao.findAsBlockIntersections(number, number);
+        if (CollectionUtils.isEmpty(asBlockList)) {
             throw new AuthenticationFailedException(UpdateMessages.noParentAsBlockFound(pkey));
         }
 
-        AttributeType attributeType = AttributeType.MNT_LOWER;
-        Collection<CIString> parentMnts = asBlock.getValuesForAttribute(attributeType);
-        if (parentMnts.isEmpty()) {
-            attributeType = AttributeType.MNT_BY;
-            parentMnts = asBlock.getValuesForAttribute(attributeType);
+        List<Message> authenticationErrorMessages = Lists.newArrayList();
+
+        final Set<RpslObject> authenticatedBy = Sets.newHashSet();
+        for (RpslObject asBlock : asBlockList) {
+            AttributeType attributeType = AttributeType.MNT_LOWER;
+            Collection<CIString> parentMnts = asBlock.getValuesForAttribute(attributeType);
+            if (parentMnts.isEmpty()) {
+                attributeType = AttributeType.MNT_BY;
+                parentMnts = asBlock.getValuesForAttribute(attributeType);
+            }
+
+            final List<RpslObject> maintainers = objectDao.getByKeys(ObjectType.MNTNER, parentMnts);
+            final List<RpslObject> authenticatedByEntry = authenticationModule.authenticate(update, updateContext, maintainers);
+            if (!authenticatedByEntry.isEmpty()) {
+                authenticatedBy.addAll(authenticatedByEntry);
+            } else {
+                authenticationErrorMessages.add(UpdateMessages.authenticationFailed(asBlock, attributeType, maintainers));
+            }
         }
 
-        final List<RpslObject> maintainers = objectDao.getByKeys(ObjectType.MNTNER, parentMnts);
-        final List<RpslObject> authenticatedBy = authenticationModule.authenticate(update, updateContext, maintainers);
         if (authenticatedBy.isEmpty()) {
-            throw new AuthenticationFailedException(UpdateMessages.authenticationFailed(asBlock, attributeType, maintainers));
+            throw new AuthenticationFailedException(authenticationErrorMessages);
         }
-        return authenticatedBy;
+
+        return Lists.newArrayList(authenticatedBy);
     }
 }
