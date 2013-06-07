cbcb149f3d8596554e48bcca2d16ac0c7cce4449
dragank <dragan@apnic.net>
2013-06-07 15:55:01 +1000

    SCRUM-1549 - Test? case for AutnumAuthentication

--- whois-update/src/test/java/net/apnic/db/whois/update/authentication/strategy/AutnumAuthenticationTest.java
+++ whois-update/src/test/java/net/apnic/db/whois/update/authentication/strategy/AutnumAuthenticationTest.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.anyCollection;
 import static org.mockito.Matchers.eq;
@@ -158,96 +157,65 @@ public class AutnumAuthenticationTest {
     }
 
     @Test
-    public void authenticated_by_mntby_x_succeeds() {
-        List<RpslObject> emptyRplsList = Lists.newArrayList();
-        when(update.getUpdatedObject()).thenReturn(RpslObject.parse("aut-num: AS3111"));
+    public void authenticated_by_mnt_low_parent_succeeds() {
+        ArrayList<RpslObject> emptyRplsList = Lists.newArrayList();
+        when(update.getUpdatedObject()).thenReturn(RpslObject.parse("aut-num: AS3211"));
 
         List<RpslObject> returnRplsList = Lists.newArrayList();
 
         returnRplsList.add(RpslObject.parse("as-block: AS3109 - AS5000\nmnt-by: BY-MNT"));
         returnRplsList.add(RpslObject.parse("as-block: AS3209 - AS3353\nmnt-lower: LOW-MNT"));
 
-        when(objectDao.findAsBlockIntersections(3111, 3111)).thenReturn(returnRplsList);
+        when(objectDao.findAsBlockIntersections(3211, 3211)).thenReturn(returnRplsList);
 
-        final ArrayList<RpslObject> parentMaintainers = Lists.newArrayList(RpslObject.parse("mntner: XLOW-MNT"));
-        when(objectDao.getByKeys(eq(ObjectType.MNTNER), anyCollection())).thenReturn(parentMaintainers);
-        when(authenticationModule.authenticate(update, updateContext, parentMaintainers)).thenReturn(emptyRplsList);
+        final ArrayList<RpslObject> parentMaintainersLow = Lists.newArrayList(RpslObject.parse("mntner: LOW-MNT"));
+        final ArrayList<RpslObject> parentMaintainersBy = Lists.newArrayList(RpslObject.parse("mntner: BY-MNT"));
+        when(objectDao.getByKeys(eq(ObjectType.MNTNER), anyCollection())).thenReturn(parentMaintainersLow);
+        when(authenticationModule.authenticate(update, updateContext, parentMaintainersLow)).thenReturn(parentMaintainersLow);
+        when(authenticationModule.authenticate(update, updateContext, parentMaintainersBy)).thenReturn(emptyRplsList);
 
-        try {
-            final List<RpslObject> authenticatedBy = subject.authenticate(update, updateContext);
-            throw new UnsupportedOperationException("should not get here");
-        } catch (AuthenticationFailedException afe) {
-            assertEquals(afe.getAuthenticationMessages().size(), 2);
-            LOGGER.info("Error: " + afe.getAuthenticationMessages().get(0).toString());
-            LOGGER.info("Error: " + afe.getAuthenticationMessages().get(1).toString());
-        }
-
-       // assertThat(authenticatedBy.equals(parentMaintainers), is(true));
+        //exception.expect(AuthenticationFailedException.class);
+        final List<RpslObject> authenticatedBy = subject.authenticate(update, updateContext);
         verifyZeroInteractions(updateContext);
     }
 
-//    @Test(expected = AuthenticationFailedException.class)
-//    public void authenticated_by_mntlower_fails() {
-//        when(update.getUpdatedObject()).thenReturn(RpslObject.parse("aut-num: AS3333"));
-//        when(objectDao.findAsBlock(3333, 3333)).thenReturn(RpslObject.parse("as-block: AS3209 - AS3353\nmnt-by: TEST-MNT\nmnt-lower: LOW-MNT"));
-//
-//        final ArrayList<RpslObject> parentMaintainers = Lists.newArrayList(RpslObject.parse("mntner: LOW-MNT"));
-//        when(objectDao.getByKeys(eq(ObjectType.MNTNER), anyCollection())).thenReturn(parentMaintainers);
-//        when(authenticationModule.authenticate(update, updateContext, parentMaintainers)).thenReturn(Lists.<RpslObject>newArrayList());
-//
-//        subject.authenticate(update, updateContext);
-//    }
-//
-//    @Test(expected = AuthenticationFailedException.class)
-//    public void cant_find_asblock() {
-//        when(update.getUpdatedObject()).thenReturn(RpslObject.parse("aut-num: AS3333"));
-//        when(objectDao.findAsBlock(3333, 3333)).thenReturn(null);
-//
-//        subject.authenticate(update, updateContext);
-//    }
-//
-//    @Test
-//    public void authenticated_by_mntby_succeeds() {
-//        when(update.getUpdatedObject()).thenReturn(RpslObject.parse("aut-num: AS3333"));
-//        when(objectDao.findAsBlock(3333, 3333)).thenReturn(RpslObject.parse("as-block: AS3209 - AS3353\nmnt-by: TEST-MNT"));
-//
-//        final ArrayList<RpslObject> parentMaintainers = Lists.newArrayList(RpslObject.parse("mntner: TEST-MNT"));
-//        when(objectDao.getByKeys(eq(ObjectType.MNTNER), anyCollection())).thenReturn(parentMaintainers);
-//        when(authenticationModule.authenticate(update, updateContext, parentMaintainers)).thenReturn(parentMaintainers);
-//
-//        final List<RpslObject> authenticatedBy = subject.authenticate(update, updateContext);
-//
-//        assertThat(authenticatedBy.equals(parentMaintainers), is(true));
-//        verifyZeroInteractions(updateContext);
-//    }
-//
-//    @Test(expected = AuthenticationFailedException.class)
-//    public void authenticated_by_mntby_fails() {
-//        when(update.getUpdatedObject()).thenReturn(RpslObject.parse("aut-num: AS3333"));
-//        when(objectDao.findAsBlock(3333, 3333)).thenReturn(RpslObject.parse("as-block: AS3209 - AS3353\nmnt-by: TEST-MNT"));
-//
-//        final ArrayList<RpslObject> parentMaintainers = Lists.newArrayList(RpslObject.parse("mntner: TEST-MNT"));
-//        when(objectDao.getByKeys(eq(ObjectType.MNTNER), anyCollection())).thenReturn(parentMaintainers);
-//        when(authenticationModule.authenticate(update, updateContext, parentMaintainers)).thenReturn(Lists.<RpslObject>newArrayList());
-//
-//        subject.authenticate(update, updateContext);
-//    }
-//
-//    @Test
-//    public void max_value() {
-//        final long maxValue = (1L << 32) - 1;
-//        final RpslObject autNum = RpslObject.parse("aut-num: AS" + maxValue);
-//        final RpslObject asBlock = RpslObject.parse("as-block: AS" + (maxValue - 1) + " - AS" + maxValue + "\nmnt-by: TEST-MNT");
-//
-//        when(update.getUpdatedObject()).thenReturn(autNum);
-//        when(objectDao.findAsBlock(maxValue, maxValue)).thenReturn(asBlock);
-//
-//        final ArrayList<RpslObject> parentMaintainers = Lists.newArrayList(RpslObject.parse("mntner: TEST-MNT"));
-//        when(objectDao.getByKeys(eq(ObjectType.MNTNER), anyCollection())).thenReturn(parentMaintainers);
-//        when(authenticationModule.authenticate(update, updateContext, parentMaintainers)).thenReturn(parentMaintainers);
-//
-//        subject.authenticate(update, updateContext);
-//
-//        verifyZeroInteractions(updateContext);
-//    }
+    @Test
+    public void authenticated_by_mnt_low_parent_fails() {
+        ArrayList<RpslObject> emptyRplsList = Lists.newArrayList();
+        when(update.getUpdatedObject()).thenReturn(RpslObject.parse("aut-num: AS3111"));
+
+        List<RpslObject> returnRplsList = Lists.newArrayList();
+
+        returnRplsList.add(RpslObject.parse("as-block: AS3109 - AS5000"));
+        returnRplsList.add(RpslObject.parse("as-block: AS3209 - AS3353\nmnt-lower: LOW-MNT"));
+
+        when(objectDao.findAsBlockIntersections(3111, 3111)).thenReturn(returnRplsList);
+
+        final ArrayList<RpslObject> parentMaintainersBy = Lists.newArrayList(RpslObject.parse("mntner: BY-MNT"));
+        final ArrayList<RpslObject> parentMaintainersLow = Lists.newArrayList(RpslObject.parse("mntner: LOW-MNT"));
+        when(objectDao.getByKeys(eq(ObjectType.MNTNER), anyCollection())).thenReturn(parentMaintainersBy);
+        when(authenticationModule.authenticate(update, updateContext, parentMaintainersLow)).thenReturn(emptyRplsList);
+        when(authenticationModule.authenticate(update, updateContext, parentMaintainersBy)).thenReturn(emptyRplsList);
+
+        exception.expect(AuthenticationFailedException.class);
+        final List<RpslObject> authenticatedBy = subject.authenticate(update, updateContext);
+    }
+
+    @Test
+    public void max_value() {
+        final long maxValue = (1L << 32) - 1;
+        final RpslObject autNum = RpslObject.parse("aut-num: AS" + maxValue);
+        final RpslObject asBlock = RpslObject.parse("as-block: AS" + (maxValue - 1) + " - AS" + maxValue + "\nmnt-by: TEST-MNT");
+
+        when(update.getUpdatedObject()).thenReturn(autNum);
+        when(objectDao.findAsBlockIntersections(maxValue, maxValue)).thenReturn(Lists.newArrayList(asBlock));
+
+        final ArrayList<RpslObject> parentMaintainers = Lists.newArrayList(RpslObject.parse("mntner: TEST-MNT"));
+        when(objectDao.getByKeys(eq(ObjectType.MNTNER), anyCollection())).thenReturn(parentMaintainers);
+        when(authenticationModule.authenticate(update, updateContext, parentMaintainers)).thenReturn(parentMaintainers);
+
+        subject.authenticate(update, updateContext);
+
+        verifyZeroInteractions(updateContext);
+    }
 }
