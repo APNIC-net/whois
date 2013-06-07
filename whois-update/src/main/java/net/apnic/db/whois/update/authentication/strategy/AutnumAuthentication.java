c7412b139bf4587e43866742cad8d32da1700d6a
dragank <dragan@apnic.net>
2013-06-07 12:05:21 +1000

    Reverting changes again due to merge

--- whois-update/src/main/java/net/apnic/db/whois/update/authentication/strategy/AutnumAuthentication.java
+++ whois-update/src/main/java/net/apnic/db/whois/update/authentication/strategy/AutnumAuthentication.java
@@ -22,7 +22,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
