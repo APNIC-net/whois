e2aed639dd1098a2953dfdf4677d31ab298ba428
dragank <dragan@apnic.net>
2013-06-18 17:29:15 +1000

    SCRUM-1547 - Inet*num APNIC validation checks process check in

--- whois-commons/src/test/java/net/apnic/db/whois/common/domain/attrs/InetnumStatusTest.java
+++ whois-commons/src/test/java/net/apnic/db/whois/common/domain/attrs/InetnumStatusTest.java
@@ -4,7 +4,7 @@ import net.ripe.db.whois.common.domain.CIString;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static net.apnic.db.whois.common.domain.attrs.Inet6numStatus.*;
+import static net.apnic.db.whois.common.domain.attrs.InetnumStatus.*;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
@@ -21,13 +21,13 @@ public class InetnumStatusTest {
 
     @Test
     public void getStatusFor() {
-        assertThat(Inet6numStatus.getStatusFor(CIString.ciString("ALLOCATED NON-PORTABLE")), is(ALLOCATED_NON_PORTABLE));
-        assertThat(Inet6numStatus.getStatusFor(CIString.ciString("ALLOCATED PORTABLE")), is(ALLOCATED_PORTABLE));
-        assertThat(Inet6numStatus.getStatusFor(CIString.ciString("ASSIGNED PORTABLE")), is(ASSIGNED_PORTABLE));
-        assertThat(Inet6numStatus.getStatusFor(CIString.ciString("ASSIGNED NON-PORTABLE")), is(ASSIGNED_NON_PORTABLE));
+        assertThat(InetnumStatus.getStatusFor(CIString.ciString("ALLOCATED NON-PORTABLE")), is(ALLOCATED_NON_PORTABLE));
+        assertThat(InetnumStatus.getStatusFor(CIString.ciString("ALLOCATED PORTABLE")), is(ALLOCATED_PORTABLE));
+        assertThat(InetnumStatus.getStatusFor(CIString.ciString("ASSIGNED PORTABLE")), is(ASSIGNED_PORTABLE));
+        assertThat(InetnumStatus.getStatusFor(CIString.ciString("ASSIGNED NON-PORTABLE")), is(ASSIGNED_NON_PORTABLE));
 
         try {
-            Inet6numStatus.getStatusFor(CIString.ciString("WHATEVER"));
+            InetnumStatus.getStatusFor(CIString.ciString("WHATEVER"));
             fail();
         } catch (Exception expected) {}
     }
