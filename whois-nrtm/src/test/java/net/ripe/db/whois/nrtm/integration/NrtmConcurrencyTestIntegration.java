75d6db33eed6d106f409dd31a7eb539ffed63f40
dragank <dragan@apnic.net>
2013-06-19 18:58:38 +1000

    Test is now reliable - Junit sometimes called @Before prematurely even though the test threads haven't finished

--- whois-nrtm/src/test/java/net/ripe/db/whois/nrtm/integration/NrtmConcurrencyTestIntegration.java
+++ whois-nrtm/src/test/java/net/ripe/db/whois/nrtm/integration/NrtmConcurrencyTestIntegration.java
@@ -43,6 +43,8 @@ public class NrtmConcurrencyTestIntegration extends AbstractNrtmIntegrationBase 
     private static final int MID_RANGE = 21486049;  // 21486050 is a person in nrtm_sample.sql
     private static final int MAX_RANGE = 21486100;
 
+    private static List<NrtmTestThread> testThreads;
+
     @BeforeClass
     public static void setInterval() {
         System.setProperty("nrtm.update.interval", "1");
@@ -50,6 +52,7 @@ public class NrtmConcurrencyTestIntegration extends AbstractNrtmIntegrationBase 
 
     @Before
     public void before() throws Exception {
+        waitForRunningThreadsToFinish();
         loadSerials(0, Integer.MAX_VALUE);
         nrtmServer.start();
     }
@@ -79,6 +82,10 @@ public class NrtmConcurrencyTestIntegration extends AbstractNrtmIntegrationBase 
         String query = String.format("-g TEST:3:%d-LAST -k", MIN_RANGE + 1);
 
         NrtmTestThread thread = new NrtmTestThread(query, MIN_RANGE + 1, countDownLatch);
+
+        // junit will now wait until test finishes in @Before incase it thinks test is finished (and test threads still runnning)
+        testThreads = Lists.newArrayList(thread);
+
         thread.start();
         countDownLatch.await(5, TimeUnit.SECONDS);
         assertThat(thread.delCount, is(1));
@@ -101,6 +108,9 @@ public class NrtmConcurrencyTestIntegration extends AbstractNrtmIntegrationBase 
         LOGGER.info("Entering manySimultaneousClientsReadingManyObjects()");
         // 1st part: clients request MIN to LAST with -k flag, but we provide half of the available serials only
         final List<NrtmTestThread> threads = Lists.newArrayList();
+
+        // junit will now wait until test finishes in @Before incase it thinks test is finished (and test threads still runnning)
+        testThreads = threads;
         CountDownLatch countDownLatch = new CountDownLatch(NUM_THREADS);
 
         setSerial(MIN_RANGE, MID_RANGE);
@@ -177,6 +187,17 @@ public class NrtmConcurrencyTestIntegration extends AbstractNrtmIntegrationBase 
         whoisTemplate.execute("TRUNCATE TABLE history");
         whoisTemplate.execute("TRUNCATE TABLE last");
     }
+
+    private void waitForRunningThreadsToFinish() {
+        if (testThreads != null) {
+            for (Thread thread : testThreads) {
+                try {
+                    thread.join();
+                } catch (InterruptedException ie) {}
+            }
+        }
+    }
+
 
     static class NrtmTestThread extends Thread {
         volatile String error;
