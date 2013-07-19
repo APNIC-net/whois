e7070ea752049bdea67c7833e36a3b0f28ef8bea
dragank <dragan@apnic.net>
2013-07-19 17:31:11 +1000

    Fixed date format for json output Pulled out duplicated helper methods - consolidated

--- whois-api/src/main/java/net/ripe/db/whois/api/whois/rdap/RdapException.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/whois/rdap/RdapException.java
@@ -1,21 +1,17 @@
 package net.ripe.db.whois.api.whois.rdap;
 
-import com.google.common.collect.Lists;
 import net.ripe.db.whois.api.whois.rdap.domain.Error;
 
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 public class RdapException {
-
-    protected static final List<String> RDAP_CONFORMANCE_LEVEL = Lists.newArrayList("rdap_level_0");
 
     public static Object build (final Response.Status status, final String selfUrl) {
         final Error exception = new Error();
         exception.setErrorCode(status.getStatusCode());
         exception.setTitle(status.getReasonPhrase());
 
-        exception.getRdapConformance().addAll(RDAP_CONFORMANCE_LEVEL);
+        exception.getRdapConformance().addAll(RdapObjectMapper.RDAP_CONFORMANCE_LEVEL);
         exception.getNotices().addAll(NoticeFactory.generateNotices(selfUrl));
 
         switch (status.getStatusCode()) {
