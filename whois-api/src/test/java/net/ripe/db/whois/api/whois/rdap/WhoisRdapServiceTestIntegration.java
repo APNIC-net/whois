d9c081098b146f98e5f0cea58e6b8f4d1c0f8895
Tom Harrison <tomh@apnic.net>
2013-07-28 00:08:53 +1000

    type no longer set for asns

--- whois-api/src/test/java/net/ripe/db/whois/api/whois/rdap/WhoisRdapServiceTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/whois/rdap/WhoisRdapServiceTestIntegration.java
@@ -751,7 +751,6 @@ public class WhoisRdapServiceTestIntegration extends AbstractRestClientTest {
         assertThat(autnum.getStartAutnum(), equalTo(123L));
         assertThat(autnum.getEndAutnum(), equalTo(123L));
         assertThat(autnum.getName(), equalTo("AS-TEST"));
-        assertThat(autnum.getType(), equalTo("DIRECT ALLOCATION"));
         assertThat(autnum.getCountry(), equalTo("AU"));
 
         final List<Event> events = autnum.getEvents();
@@ -789,7 +788,6 @@ public class WhoisRdapServiceTestIntegration extends AbstractRestClientTest {
         assertThat(autnum.getStartAutnum(), equalTo(1234L));
         assertThat(autnum.getEndAutnum(), equalTo(1234L));
         assertThat(autnum.getName(), equalTo("AS-TEST"));
-        assertThat(autnum.getType(), equalTo("DIRECT ALLOCATION"));
 
         final List<Event> events = autnum.getEvents();
         assertThat(events, hasSize(1));
