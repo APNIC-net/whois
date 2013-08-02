2ad14f0b03f663edfe01a2a77bc36e645241e07c
Gary Kennedy <gary@apnic.net>
2013-08-02 15:34:30 +1000

    whoisdb changes

---
+++ whois-commons/src/main/resources/patch/apnic_whoisdb_patches_1.68.sql
@@ -0,0 +1,41 @@
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `object_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `tag_id` varchar(50) NOT NULL DEFAULT '',
+  `data` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`object_id`, `tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `update_lock`
+--
+
+DROP TABLE IF EXISTS `update_lock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `update_lock` (
+  `global_lock` int(11) NOT NULL,
+  PRIMARY KEY (`global_lock`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `version`
+--
+
+DROP TABLE IF EXISTS `version`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `version` (
+  `version` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+INSERT INTO `version` VALUES ('whois-1.61');
