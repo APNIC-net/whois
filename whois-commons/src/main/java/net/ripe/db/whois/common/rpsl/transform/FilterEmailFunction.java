40fa457c141d79158adcccaa74bd5d928d076d1e
Gary Kennedy <gary@apnic.net>
2013-08-06 15:37:53 +1000

    filtering of auth/email is not controlled by the filtering flag - we now skip filtering for apnic variant

--- whois-commons/src/main/java/net/ripe/db/whois/common/rpsl/transform/FilterEmailFunction.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/rpsl/transform/FilterEmailFunction.java
@@ -1,6 +1,7 @@
 package net.ripe.db.whois.common.rpsl.transform;
 
 import com.google.common.collect.Sets;
+import net.ripe.db.whois.common.profiles.WhoisVariant;
 import net.ripe.db.whois.common.rpsl.AttributeType;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 import net.ripe.db.whois.common.rpsl.RpslObjectFilter;
@@ -20,6 +21,10 @@ public class FilterEmailFunction implements FilterFunction {
 
     @Override
     public RpslObject apply(RpslObject rpslObject) {
+        if (WhoisVariant.isAPNIC()) {
+            return rpslObject;
+        }
+
         RpslObject filtered = RpslObjectFilter.removeAttributeTypes(rpslObject, filterAttributes);
         return filtered == rpslObject ? rpslObject : RpslObjectFilter.setFiltered(filtered);
     }
