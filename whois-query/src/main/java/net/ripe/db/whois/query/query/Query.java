fbe3f3a1d4927a77b9e70646abd493e04f4e1390
Gary Kennedy <gary@apnic.net>
2013-08-21 11:26:50 +1000

    [SCRUM-1768] Forgot an import. (Everything still merged/compiled so I'm confused)

--- whois-query/src/main/java/net/ripe/db/whois/query/query/Query.java
+++ whois-query/src/main/java/net/ripe/db/whois/query/query/Query.java
@@ -15,6 +15,7 @@ import net.ripe.db.whois.common.Messages;
 import net.ripe.db.whois.common.domain.CIString;
 import net.ripe.db.whois.common.domain.IpInterval;
 import net.ripe.db.whois.common.domain.attrs.AsBlockRange;
+import net.ripe.db.whois.common.profiles.WhoisVariant;
 import net.ripe.db.whois.common.rpsl.AttributeType;
 import net.ripe.db.whois.common.rpsl.ObjectTemplate;
 import net.ripe.db.whois.common.rpsl.ObjectType;
