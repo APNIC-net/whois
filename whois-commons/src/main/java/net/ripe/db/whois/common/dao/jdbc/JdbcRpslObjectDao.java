337fdc6e3085ebedf70acac8feadae1091e3340d
Tom Harrison <tomh@apnic.net>
2013-09-16 09:36:33 +1000

    throw "correct" exception in getById

--- whois-commons/src/main/java/net/ripe/db/whois/common/dao/jdbc/JdbcRpslObjectDao.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/dao/jdbc/JdbcRpslObjectDao.java
@@ -9,6 +9,7 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import com.google.common.util.concurrent.UncheckedExecutionException;
 import net.ripe.db.whois.common.aspects.RetryFor;
 import net.ripe.db.whois.common.dao.RpslObjectDao;
 import net.ripe.db.whois.common.dao.RpslObjectInfo;
@@ -29,6 +30,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.dao.RecoverableDataAccessException;
@@ -209,7 +211,18 @@ public class JdbcRpslObjectDao implements RpslObjectDao {
 
     @Override
     public RpslObject getById(final int objectId) {
-        return getKeyObjectCacheForCurrentSource().getUnchecked(objectId);
+        RpslObject result;
+        try {
+            result = getKeyObjectCacheForCurrentSource().getUnchecked(objectId);
+        } catch (UncheckedExecutionException e) {
+            Throwable throwable = e.getCause();
+            if (throwable instanceof DataAccessException) {
+                throw (DataAccessException) throwable;
+            } else {
+                throw e;
+            }
+        }
+        return result;
     }
 
     @Override
